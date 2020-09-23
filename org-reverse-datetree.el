22ebfaceeca36af0d12714e515aee222bd23e402
Akira Komamura <akira.komamura@gmail.com>
2020-09-23 17:56:00 +0900

    Bump to 0.3.4

--- org-reverse-datetree.el
+++ org-reverse-datetree.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2018-2020 Akira Komamura
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
-;; Version: 0.3.3
+;; Version: 0.3.4
 ;; Package-Requires: ((emacs "26.1") (dash "2.12") (org "9.3"))
 ;; Keywords: outlines
 ;; URL: https://github.com/akirak/org-reverse-datetree
